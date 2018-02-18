6cc7d3ce6634df817dd73ff4d875640653e97215
TheEndurance <rawa.jalal@gmail.com>
2018-02-17 21:20:18 -0500

    search movies by name -> list view

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,37 @@
-"use strict";
+'use strict';
 
-var a = "hello";
+var httpRequest = new XMLHttpRequest();
+var searchText = document.getElementById('searchText');
+var listView = document.getElementById('movieList');
+var API = 'http://www.omdbapi.com/?apikey=7823e390';
+
+var callAPI = function callAPI(typeOfRequest, event) {
+    var textValue = event.target.value;
+    if (textValue.length < 3) return;
+    httpRequest.onreadystatechange = updateListView;
+    httpRequest.open(typeOfRequest, API + '&s=' + textValue, true);
+    httpRequest.send();
+};
+
+var updateListView = function updateListView() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            templateListView(JSON.parse(httpRequest.responseText).Search);
+        } else {
+            alert('There was a problem with the request.');
+        }
+    }
+};
+
+var templateListView = function templateListView(json) {
+    if (json === undefined) return;
+    var markup = '\n        ' + json.map(function (movie) {
+        return '<li>\n                <a href="#">\n                    <img src="' + movie.Poster + '">\n                    <h2>' + movie.Title + '</h2>\n                    <p>' + movie.Year + '</p>\n                </a>\n            </li> \n        ';
+    }).join('') + '\n    ';
+    listView.innerHTML = markup;
+    $(listView).listview('refresh');
+};
+
+searchText.addEventListener('keyup', function (e) {
+    callAPI('GET', e);
+}, false);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-let a = "hello";
+const httpRequest = new XMLHttpRequest();
+const searchText = document.getElementById('searchText');
+const listView = document.getElementById('movieList');
+const API='http://www.omdbapi.com/?apikey=7823e390';
+
+
+const callAPI = function (typeOfRequest,event) {
+    const textValue = event.target.value;
+    if (textValue.length < 3) return;
+    httpRequest.onreadystatechange = updateListView;
+    httpRequest.open(typeOfRequest, `${API}&s=${textValue}`, true)
+    httpRequest.send();
+    
+}
+
+
+const updateListView = function () {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            templateListView(JSON.parse(httpRequest.responseText).Search);
+        } else {
+            alert('There was a problem with the request.');
+        }
+    }
+}
+
+const templateListView = function(json){
+    if (json===undefined) return;
+    const markup = `
+        ${json.map(movie => 
+            `<li>
+                <a href="#">
+                    <img src="${movie.Poster}">
+                    <h2>${movie.Title}</h2>
+                    <p>${movie.Year}</p>
+                </a>
+            </li> 
+        `).join('')}
+    `;
+    listView.innerHTML = markup;
+    $(listView).listview('refresh');
+}
+
+
+searchText.addEventListener('keyup', function(e){ callAPI('GET',e)},false);
