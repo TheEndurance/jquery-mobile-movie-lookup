12269b98b14fcb9a1b68c5066aa7fcdd99ad0b33
TheEndurance <rawa.jalal@gmail.com>
2018-02-19 21:10:04 -0500

    generalize callAPI, update function names, clean up code

--- dist/index.js
+++ dist/index.js
@@ -4,34 +4,83 @@ var httpRequest = new XMLHttpRequest();
 var searchText = document.getElementById('searchText');
 var listView = document.getElementById('movieList');
 var API = 'http://www.omdbapi.com/?apikey=7823e390';
+var MOVIE_CLICK_CLASS = "js-click-movie";
 
-var callAPI = function callAPI(typeOfRequest, event) {
-    var textValue = event.target.value;
-    if (textValue.length < 3) return;
-    httpRequest.onreadystatechange = updateListView;
-    httpRequest.open(typeOfRequest, API + '&s=' + textValue, true);
-    httpRequest.send();
+///
+// Utility function to call API
+///
+var callAPI = function callAPI() {
+    var typeOfRequest = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'GET';
+    var callback = arguments[1];
+    var apiUrl = arguments[2];
+
+    for (var _len = arguments.length, queryStrings = Array(_len > 3 ? _len - 3 : 0), _key = 3; _key < _len; _key++) {
+        queryStrings[_key - 3] = arguments[_key];
+    }
+
+    if (typeof callback === 'function' && Array.isArray(queryStrings)) {
+        httpRequest.onreadystatechange = callback;
+        httpRequest.open(typeOfRequest, '' + apiUrl + queryStrings.map(function (queryString) {
+            return '&' + queryString;
+        }), true);
+        httpRequest.send();
+        console.log();
+    }
 };
 
-var updateListView = function updateListView() {
+///
+// Event handler for searching movies
+///
+var searchMovieEventHandler = function searchMovieEventHandler() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
-            templateListView(JSON.parse(httpRequest.responseText).Search);
+            var markup = renderMovieListItems(JSON.parse(httpRequest.responseText).Search);
+            if (markup) {
+                listView.innerHTML = markup;
+                $(listView).listview('refresh');
+            }
         } else {
             alert('There was a problem with the request.');
         }
     }
 };
 
-var templateListView = function templateListView(json) {
-    if (json === undefined) return;
-    var markup = '\n        ' + json.map(function (movie) {
-        return '<li>\n                <a href="#">\n                    <img src="' + movie.Poster + '">\n                    <h2>' + movie.Title + '</h2>\n                    <p>' + movie.Year + '</p>\n                </a>\n            </li> \n        ';
+///
+// Renders list items for the movies
+///
+var renderMovieListItems = function renderMovieListItems(jsonArray) {
+    if (jsonArray === undefined) return;
+    return '\n        ' + jsonArray.map(function (movie) {
+        return '<li>\n                <a class="' + MOVIE_CLICK_CLASS + '" data-movie-id="' + movie.imdbID + '" href="#">\n                    <img src="' + movie.Poster + '">\n                    <h2>' + movie.Title + '</h2>\n                    <p>' + movie.Year + '</p>\n                </a>\n            </li> \n        ';
     }).join('') + '\n    ';
-    listView.innerHTML = markup;
-    $(listView).listview('refresh');
 };
 
-searchText.addEventListener('keyup', function (e) {
-    callAPI('GET', e);
+///
+// Event handler when movie list items are clicked
+///
+var movieClickedEventHandler = function movieClickedEventHandler() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            window.sessionStorage.setItem('movie', httpRequest.responseText);
+        } else {
+            alert('There was a problem with the request.');
+        }
+    }
+};
+
+///
+// Event listeners
+///
+searchText.addEventListener('keydown', function (e) {
+    var searchValue = e.target.value;
+    if (searchValue.length > 3) {
+        callAPI('GET', searchMovieEventHandler, API, ['s=' + searchValue]);
+    }
 }, false);
+
+$('#movieList').on('click', 'a.' + MOVIE_CLICK_CLASS, function () {
+    var movieId = this.dataset.movieId;
+    if (movieId && movieId.length > 0) {
+        callAPI('GET', movieClickedEventHandler, API, ['i=' + movieId]);
+    }
+});
--- src/index.js
+++ src/index.js
@@ -1,35 +1,48 @@
 const httpRequest = new XMLHttpRequest();
 const searchText = document.getElementById('searchText');
 const listView = document.getElementById('movieList');
-const API='http://www.omdbapi.com/?apikey=7823e390';
+const API = 'http://www.omdbapi.com/?apikey=7823e390';
+const MOVIE_CLICK_CLASS = "js-click-movie";
 
 
-const callAPI = function (typeOfRequest,event) {
-    const textValue = event.target.value;
-    if (textValue.length < 3) return;
-    httpRequest.onreadystatechange = updateListView;
-    httpRequest.open(typeOfRequest, `${API}&s=${textValue}`, true)
-    httpRequest.send();
-    
+///
+// Utility function to call API
+///
+const callAPI = function (typeOfRequest = 'GET', callback, apiUrl, ...queryStrings) {
+    if (typeof callback === 'function' && Array.isArray(queryStrings)) {
+        httpRequest.onreadystatechange = callback;
+        httpRequest.open(typeOfRequest, `${apiUrl}${queryStrings.map(queryString=>`&${queryString}`)}`, true)
+        httpRequest.send();
+        console.log();
+    }
 }
 
-
-const updateListView = function () {
+///
+// Event handler for searching movies
+///
+const searchMovieEventHandler = function () {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
-            templateListView(JSON.parse(httpRequest.responseText).Search);
+            const markup = renderMovieListItems(JSON.parse(httpRequest.responseText).Search);
+            if (markup) {
+                listView.innerHTML = markup;
+                $(listView).listview('refresh');
+            }
         } else {
             alert('There was a problem with the request.');
         }
     }
 }
 
-const templateListView = function(json){
-    if (json===undefined) return;
-    const markup = `
-        ${json.map(movie => 
+///
+// Renders list items for the movies
+///
+const renderMovieListItems = function (jsonArray) {
+    if (jsonArray === undefined) return;
+    return `
+        ${jsonArray.map(movie => 
             `<li>
-                <a href="#">
+                <a class="${MOVIE_CLICK_CLASS}" data-movie-id="${movie.imdbID}" href="#">
                     <img src="${movie.Poster}">
                     <h2>${movie.Title}</h2>
                     <p>${movie.Year}</p>
@@ -37,9 +50,36 @@ const templateListView = function(json){
             </li> 
         `).join('')}
     `;
-    listView.innerHTML = markup;
-    $(listView).listview('refresh');
+}
+
+///
+// Event handler when movie list items are clicked
+///
+const movieClickedEventHandler = function () {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            window.sessionStorage.setItem('movie', httpRequest.responseText);
+        } else {
+            alert('There was a problem with the request.');
+        }
+    }
 }
 
 
-searchText.addEventListener('keyup', function(e){ callAPI('GET',e)},false);
+///
+// Event listeners
+///
+searchText.addEventListener('keydown', function (e) {
+    const searchValue = e.target.value;
+    if (searchValue.length > 3) {
+        callAPI('GET', searchMovieEventHandler, API, [`s=${searchValue}`])
+    }
+}, false);
+
+
+$('#movieList').on('click', `a.${MOVIE_CLICK_CLASS}`, function () {
+    const movieId = this.dataset.movieId
+    if (movieId && movieId.length > 0) {
+        callAPI('GET', movieClickedEventHandler, API, [`i=${movieId}`]);
+    }
+});
